eaf501ae0894cef3092080a57dd4de09e596dbbf
mdomzol <domzolmichal@gmail.com>
2026-03-25 22:17:33 +0100

    GUI hotfix

--- Preparator - source/MainWindow.xaml.cs
+++ Preparator - source/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 using Preparator.ViewModels;
 
 namespace Preparator
@@ -9,6 +11,40 @@ namespace Preparator
         {
             InitializeComponent();
             DataContext = new MainViewModel();
+        }
+
+        private void TopBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                ToggleMaximize();
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleMaximize();
+        }
+
+        private void ToggleMaximize()
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
