5b93e589dd70a49ca5e508b0ea58c5a87aceadd3
mdomzol <domzolmichal@gmail.com>
2026-04-17 08:23:20 +0200

    PresetItem creation

---
+++ Preparator - source/Models/PresetItem.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Preparator.Models
+{
+    public class PresetItem : INotifyPropertyChanged
+    {
+        public bool _isSelected;
+        public string Name { get; set; }
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
+
+
